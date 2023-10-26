7c5ca0742c90ce7994a6b188872f8b6710a48dc8
pash1 <pash1@PASH>
2023-10-26 19:30:32 +0300

    fix firebase need add user exit , email exit login eror email or password dont match

--- js/firebase.js
+++ js/firebase.js
@@ -1,23 +1,45 @@
-// firebase.js
+// Import the functions you need from the SDKs you need
 
-// Your Firebase config object, obtained from your Firebase project settings
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-analytics.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
 
     apiKey: "AIzaSyCMtz8eJe7WYvllOQ0wtSyOuW_a7gUkG1c",
-  
+
     authDomain: "see-the-sea-4c396.firebaseapp.com",
-  
+
+    databaseURL: "https://see-the-sea-4c396-default-rtdb.europe-west1.firebasedatabase.app",
+
     projectId: "see-the-sea-4c396",
-  
+
     storageBucket: "see-the-sea-4c396.appspot.com",
-  
+
     messagingSenderId: "208499675243",
-  
+
     appId: "1:208499675243:web:ff025b1f1d549ad6a0612e",
-  
+
     measurementId: "G-TNLP4SHRHW"
-  
-  };
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -28,57 +50,193 @@ document.getElementById('registerForm').addEventListener('submit', function (eve
 
     var username = document.getElementById('username').value;
     var email = document.getElementById('email').value;
+    var confirmEmail = document.getElementById('ConfirmEmail');
     var password = document.getElementById('password').value;
+    var confirmPassword = document.getElementById('confirmPassword').value;
+    var productSerial = document.getElementById('productSerial').value;
+    var errorElement = document.getElementById('error');
 
-    // Firebase registration
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then(function (userCredential) {
-            // User registered successfully
-            var user = userCredential.user;
-            console.log('User registered:', user);
+    switch (checkGood(username, email, password, confirmPassword, confirmEmail, productSerial)) {
+        case 0:
+            // Firebase registration
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+                .then(function (userCredential) {
+                    // User registered successfully
+                    var user = userCredential.user;
+                    console.log('User registered:', user);
 
-            // Additional code to store user data, if needed
-            // For example, storing the username in Firestore
-            firebase.firestore().collection('users').doc(user.uid).set({
-                username: username,
-                email: email,
-                // Other user data you want to store
-            }).then(function () {
-                console.log('User data stored successfully');
-            }).catch(function (error) {
-                console.error('Error storing user data:', error);
-            });
-        })
-        .catch(function (error) {
-            // Handle registration errors
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.error('Registration error:', errorCode, errorMessage);
-        });
+                    // Additional code to store user data, if needed
+                    // For example, storing the username in Firestore
+                    firebase.firestore().collection('users').doc(user.uid).set({
+                        username: username,
+                        email: email,
+                        // Other user data you want to store
+                    }).then(function () {
+                        console.log('User data stored successfully');
+                    }).catch(function (error) {
+                        console.error('Error storing user data:', error);
+                    });
+                })
+                .catch(function (error) {
+                    // Handle registration errors
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    console.error('Registration error:', errorCode, errorMessage);
+                });
+            break;
+        case 1:
+            errorMessage.textContent = "Username field is empty";
+            break;
+        case 2:
+            errorMessage.textContent = "Username must have only a-z, A-Z, and 0-9";
+            break;
+        case 3:
+            errorMessage.textContent = "Email field is empty";
+            break;
+        case 4:
+            errorMessage.textContent = "Email is not valid";
+            break;
+        case 5:
+            errorMessage.textContent = "Confirm Email field is empty";
+            break;
+        case 6:
+            errorMessage.textContent = "Email and Confirm Email do not match";
+            break;
+        case 7:
+            errorMessage.textContent = "Password field is empty";
+            break;
+        case 8:
+            errorMessage.textContent = "Password must have a-z, A-Z, 0-9, and at least one symbol";
+            break;
+        case 9:
+            errorMessage.textContent = "Confirm Password field is empty";
+            break;
+        case 10:
+            errorMessage.textContent = "Password and Confirm Password do not match";
+            break;
+        case 11:
+            errorMessage.textContent = "Serial number is invalid or does not exist";
+            break;
+        default:
+            errorMessage.textContent = "An unknown error occurred";
+            break;
+    }
 });
 
+
+// Login script
 document.getElementById('loginForm').addEventListener('submit', function (event) {
     event.preventDefault();
 
-    var email = document.getElementById('email').value; // Updated to use email
+    var email = document.getElementById('email').value; // Assuming you're using the username field for email
     var password = document.getElementById('password').value;
+    switch (checkLogin(email, password)) {
+        case 0:
+            // Firebase login
+            firebase.auth().signInWithEmailAndPassword(email, password)
+                .then(function (userCredential) {
+                    // User logged in successfully
+                    var user = userCredential.user;
+                    console.log('User logged in:', user);
 
-    // Firebase login
-    firebase.auth().signInWithEmailAndPassword(email, password)
-        .then(function (userCredential) {
-            // User logged in successfully
-            var user = userCredential.user;
-            console.log('User logged in:', user);
-
-            // Redirect to a specific page after successful login if desired
-            // For example:
-            // window.location.href = "dashboard.html"; 
-        })
-        .catch(function (error) {
-            // Handle login errors
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.error('Login error:', errorCode, errorMessage);
-            alert('Login failed: ' + errorMessage);  // Inform the user
-        });
+                    // Redirect to a specific page after successful login if desired
+                    // For example:
+                    // window.location.href = "dashboard.html"; 
+                })
+                .catch(function (error) {
+                    // Handle login errors
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    console.error('Login error:', errorCode, errorMessage);
+                    alert('Login failed: ' + errorMessage);  // Inform the user
+                });
+            break;
+        case 1:
+            errorMessage.textContent = "Email field is empty";
+            break;
+        case 2:
+            errorMessage.textContent = "Password field is empty";
+            break;
+        default:
+            errorMessage.textContent = "An unknown error occurred";
+            break;
+    }
 });
+
+function checkGoodReg(username, mail, password, confPass, confEmail, ser) {
+    if (!username) {
+        return 1;
+    }
+    else if (!userNmaeGood(username)) {
+        return 2;       // user name must have only a-z A-Z and 0-9
+    }
+    else if (!mail) {
+        return 3; // Email field is empty
+    } else if (!checkIfEmailValid(mail)) {
+        return 4; // Email is not valid
+    }
+    else if (!confEmail) {
+        return 5;
+    }
+    else if (checkEmailAndConfEmail(email, confEmail)) {
+        return 6; //email and conf email !=
+    }
+    else if (!password) {
+        return 7; // Password field is empty
+    }
+    else if (!checkPassword(password)) {
+        return 8;   // password dont leget need to have a-z A-Z 0-9 and syblome
+    }
+    else if (!confPass) {
+        return 9; //Conf Pass field is empty
+    }
+    else if (checkPassToPassConf(password, confPass)) {
+        return 10  //pass and conf !=
+    }
+    else if (!SerNum(ser)) {
+        return 11;;            // serial number dont exit 
+    }
+    return 0; // Everything is okay
+}
+
+function checkLogin(email, password) {
+    if (!email) {
+        return 1;
+    }
+    else if (!password) {
+        return 2;
+    }
+    return 0;
+}
+
+function checkIfEmailValid(email,) {
+    var validEmailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+    return validEmailRegex.test(email);
+}
+
+function checkEmailAndConfEmail(email, confEmail) {
+    return email === confPass;
+}
+
+function checkPassToPassConf(pass, confPass) {
+    return pass === confPass;
+}
+
+function userNmaeGood(userName) {
+    var pattern = /^[a-zA-Z0-9]+$/;
+    return pattern.test(userName);
+
+}
+
+function checkPassword(pass) {
+    var pattern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+    return pattern.test(pass);
+}
+function SerNum(ser) {
+    var pattern = /^gfsts\d{5}$/;
+    return pattern.test(ser)
+}
+
+
+
+
---
+++ js/res.js
@@ -0,0 +1,66 @@
+// Add an event listener that waits for the full HTML document to load before running the function
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the 'submitButton' element by its ID
+    var submitButton = document.getElementById('submitButton')
+    // Add a click event listener to the 'submitButton' that calls the 'registerUser' function when clicked
+    submitButton.addEventListener('click', registerUser)
+    
+});
+
+// Function to handle the user registration process
+function registerUser() {
+    // Get the values from the email and password input fields
+    var email = document.getElementById('inputValue').value;
+    var password = document.getElementById('pass').value;
+    // Get the error message element by its ID
+    var errorMessage = document.getElementById('errorMessage');
+    
+    // Define a regular expression pattern to validate the email
+    var validEmailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+    // Call the 'checkGood' function to validate the user input and take action based on the result
+    switch (checkGood(email, password, validEmailRegex)) {
+        case 0:
+            // If the check passes, add the user and redirect to 'index.html'
+            addUser(email,password)
+            window.location.href = "index.html";
+            break;
+        case 1:
+            // If the email already exists, display an error message
+            errorMessage.textContent = "bitch im taken"
+            break;
+        case 2:
+            // If the password field is empty, display an error message
+            errorMessage.textContent = "Password field is empty";
+            break;
+        case 3:
+            // If the email is not valid, display an error message
+            errorMessage.textContent = "Email is not valid";
+            break;
+        case 4:
+            // If the email field is empty, display an error message
+            errorMessage.textContent = "Email field is empty";
+            break;
+    }
+}
+
+// Function to check the validity of the user input
+function checkGood(mail, password, validEmailRegex) {
+    // Retrieve the existing users from localStorage
+    var users = getUsersFromLocalStorage();
+    
+    // Check if the email already exists among the existing users
+    var userExists = users.some(user => user.mail === mail);
+
+    // Check the conditions and return a code for each possible scenario
+    if (!mail) {
+        return 4; // Email field is empty
+    } else if (!validEmailRegex.test(mail)) {
+        return 3; // Email is not valid
+    } else if (!password) {
+        return 2; // Password field is empty
+    } else if (userExists) {
+        return 1; // Email already exists in localStorage
+    }
+    return 0; // Everything is okay
+}
