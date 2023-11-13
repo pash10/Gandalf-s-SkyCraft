7e2f24a885497189088b26a947f16a2b868789dc
pash1 <pash1@PASH>
2023-11-13 11:43:14 +0200

    fix css nav bar , fix css intro mobile, add locacl storge need to

--- js/index.js
+++ js/index.js
@@ -19,10 +19,37 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
     /*SHOW ONLY What you need in the nav bar */
+
+
+
 });
 
 
 
 
+/*need to add  */
+/*
+const homeLink = document.getElementById('homeLink');
+const droneGuideLink = document.getElementById('aboutLink');
+const contactLink = document.getElementById('contactLink');
+const productLink = document.getElementById('productLink');
+const loginLink = document.getElementById('loginLink');
+const registerLink = document.getElementById('registerLink');
+const liveViewLink = document.getElementById('liveViewLink');
+const logoutButton = document.getElementById('logoutButton');
 
-
+if(localStorage.getItem('isLoggedIn') =='true') {
+    console.log('word') // Logs 'word' to the console if the user is logged in
+    logoutButton.style.display = 'block';
+    postLink.style.display = 'block';
+    mapLink.style.display = 'blcok'; // Typo: should be 'block'
+    registerLink.style.display = 'none';
+    loginLink.style.display = 'none';
+} else {
+    logoutButton.style.display = 'none';
+    postLink.style.display = 'none';
+    mapLink.style.display = 'none';
+    registerLink.style.display = 'block';
+    loginLink.style.display = 'block';
+}
+*/
