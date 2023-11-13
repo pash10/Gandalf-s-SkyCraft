321bd386d2a3af01c9f4fa4e4ff70f837a552773
pash1 <pash1@PASH>
2023-11-13 11:20:05 +0200

    fixed the nav bar u and red, need to fix p into firebase and nav bar show if login

--- js/index.js
+++ js/index.js
@@ -1,14 +1,28 @@
-
 document.addEventListener("DOMContentLoaded", function() {
     let path = window.location.pathname;
     let page = path.split("/").pop();
+    console.log("Page from URL: ", page); // Should output 'pro.html' when on the product page.
+
     let navItems = document.querySelectorAll("#navbar a");
+
     navItems.forEach(item => {
+        console.log("Nav item href: ", item.getAttribute("href")); // Should output 'pro.html' for the product link.
         if (item.getAttribute("href") === page) {
+            console.log("Match found for: ", item.getAttribute("href")); // Check if 'pro.html' match is found.
             item.style.fontWeight = "bold";
-            item.style.fontSize = "larger";  // Makes the font size larger
-            item.style.color = "red";       // Changes the color to red
-            item.style.textDecoration = "underline"; // Adds underline
+            item.style.fontSize = "larger";
+            item.style.color = "red";
+            item.style.textDecoration = "underline";
         }
     });
+
+
+
+    /*SHOW ONLY What you need in the nav bar */
 });
+
+
+
+
+
+
